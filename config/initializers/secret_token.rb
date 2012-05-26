9641b14a741f7ce95d025b4484b086889b71371a
Lemured <john.jlassiter@gmail.com>
2012-05-26 12:57:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LmdSite::Application.config.secret_token = '6efdc590eacf19438649f1556eb81e68f607b9d7272a6547877caae2fad7ea3c4e7a35b6ab3dd30ae564bf239e866a248816c590397965cda2711bea316f755b'
